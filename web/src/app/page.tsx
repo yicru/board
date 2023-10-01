5b68d51e42a1e9bb41289c317734d5ee594978ae
yuichiro miyamae <yu1ro.mya@gmail.com>
2023-10-02 01:35:18 +0900

    update: Home

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,4 +1,5 @@
 import { StickyNote } from '@/components/app/sticky-note'
+import { Separator } from '@/components/ui/separator'
 import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
 import { cookies } from 'next/headers'
 
@@ -13,7 +14,22 @@ export default async function Home() {
       <div className={'grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3'}>
         {[...new Array(12)].map((_, i) => (
           <StickyNote key={`sticky-note-${i}`}>
-            <div className={'p-4'}>{i}</div>
+            <div className={'p-4'}>
+              <p className={'text-zinc-600 dark:text-foreground'}>Lorem ipsum</p>
+              <Separator className={'my-2 dark:bg-zinc-700'} />
+              <p className={'line-clamp-4 text-sm leading-relaxed tracking-wide text-zinc-400'}>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+                dolore magna aliqua. Netus et malesuada fames ac turpis egestas maecenas pharetra. Consequat ac felis
+                donec et odio. Nulla facilisi cras fermentum odio eu. Sit amet mauris commodo quis imperdiet massa.
+                Laoreet id donec ultrices tincidunt arcu non. Egestas pretium aenean pharetra magna ac placerat
+                vestibulum. Ornare massa eget egestas purus viverra accumsan in. Euismod nisi porta lorem mollis aliquam
+                ut porttitor leo a. Aliquam id diam maecenas ultricies mi eget mauris pharetra et. A diam maecenas sed
+                enim ut. Neque sodales ut etiam sit amet nisl. Tellus cras adipiscing enim eu turpis egestas pretium
+                aenean pharetra. Ornare arcu odio ut sem nulla pharetra. Cursus sit amet dictum sit amet justo donec
+                enim diam. Pulvinar sapien et ligula ullamcorper malesuada. Fermentum odio eu feugiat pretium nibh ipsum
+                consequat nisl vel. Fringilla est ullamcorper eget nulla facilisi etiam.
+              </p>
+            </div>
           </StickyNote>
         ))}
       </div>
--- web/src/components/app/sticky-note/index.tsx
+++ web/src/components/app/sticky-note/index.tsx
@@ -18,8 +18,8 @@ export function StickyNote(props: Props) {
       )}
       ratio={16 / 9}
     >
-      <div className={cn('absolute right-0 top-0 h-px w-[50%]', styles.line)} />
-      <div className={cn('absolute left-0 bottom-0 h-px w-[50%]', styles.line)} />
+      <div className={cn('absolute left-0 top-0 h-px w-[50%]', styles.line)} />
+      <div className={cn('absolute right-0 bottom-0 h-px w-[50%]', styles.line)} />
       <div>{props.children}</div>
     </AspectRatio>
   )
