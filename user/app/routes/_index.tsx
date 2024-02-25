aa60faf74efe8658c88182356b0cc0d8304b1276
yuichiro miyamae <yu1ro.mya@gmail.com>
2024-02-25 18:23:26 +0900

    add: shadcn

--- user/app/routes/_index.tsx
+++ user/app/routes/_index.tsx
@@ -1,4 +1,5 @@
 import type { MetaFunction } from '@remix-run/node'
+import { Button } from '~/components/ui/button'
 
 export const meta: MetaFunction = () => {
   return [
@@ -12,8 +13,8 @@ export const meta: MetaFunction = () => {
 
 export default function Index() {
   return (
-    <div className={'text-center'}>
-      <h1 className={'text-xl font-bold'}>Hello, Tailwind!</h1>
+    <div>
+      <Button>Click me</Button>
     </div>
   )
 }
